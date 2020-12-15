7e1da8ea8d1e5aff18b5d68629d7585f84be9f0f
Endy Muhardin <endy.muhardin@gmail.com>
2020-12-15 13:49:42 +0700

    route untuk ke bukutamu

--- bukutamu-laravel/routes/web.php
+++ bukutamu-laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BukutamuController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('bukutamu', BukutamuController::class);
